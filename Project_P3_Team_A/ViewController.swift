381f037c2ea490abadcd9f470ad8d4b1354e15f6
Jacob HInchey <jacob.hinchey@okstate.edu>
2019-10-22 12:04:01 -0500

    Initial Commit

---
+++ Project_P3_Team_A/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_P3_Team_A
+//
+//  Created by Jacob HInchey on 10/22/19.
+//  Copyright © 2019 Team A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
